485e641772068778e123243223382f84d19309ba
thompsizzle <thompsizzle@gmail.com>
2022-11-08 12:16:28 -0700

    add terraform for new template

---
+++ AWS/EC2_hello_world_Ansible/README.md
@@ -0,0 +1,89 @@
+# AWS EC2 Hello World template
+
+## Description
+
+Creates an AWS infrastructure with an EC2 instance, using Terraform. Returns the public IP address to the console. Ansible is provided in place of the EC2 instance user data file to configure the EC2 instance operating system and render Hello World.
+
+## Installation and Usage
+
+### Install AWS CLI
+
+In order to communicate with the AWS API, you will need to install the AWS CLI. Click on the following link for instructions:
+
+https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+
+### Configure AWS credentials
+
+Before using this template, you need to configure your AWS credentials.
+
+    aws configure
+
+For more help configuring your AWS credentials: https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-quickstart.html
+
+### Install Terraform CLI
+
+Please install Terraform on your local machine. Click on the following link for instructions:
+
+https://www.terraform.io/downloads
+
+### Create key for secure connections using SSH
+
+Run the following command to generate SSH key.
+
+    ssh-keygen -t rsa -C "example@gmail.com" -f ./ssh-key-tf
+
+Note: .gitignore has references to this key using the exact name of the key in the command above. Edit the key name, edit the references in .gitignore, under comment '# ssh keys'.
+
+### Available variables
+
+Customize Terraform variables in terraform.tfvars.
+
+AWS region:
+
+    aws_region          = "us-east-1"
+
+VPC CIDR block:
+
+    address_space       = "10.17.0.0/16"
+
+Choose 2 availability zones within region:
+
+    availability_zone_1 = "us-east-1a"
+    availability_zone_2 = "us-east-1b"
+
+Define map of amis based on region:
+
+    aws_amis = {
+        "us-east-1" = "ami-0cff7528ff583bf9a"
+        "us-east-2" = "ami-0ebc8f6f580a04647"
+        "us-west-1" = "ami-008b09448b998a562"
+        "us-west-2" = "ami-008b09448b998a562"
+    }
+
+EC2 instance type:
+
+    ec2_instance_type = "t2.micro"
+
+Be careful when choosing the EC2 instance type. This is what incurs the cost for this infrastructure.
+
+### Initialize Terraform
+
+Initialize a working directory that contains a Terraform configuration:
+
+    terraform init
+
+### Plan your Terraform
+
+Create an execution plan and preview the changes that Terraform plans to make to your infrastructure:
+
+    terraform plan
+
+### Execute your Terraform
+
+Execute the actions proposed in a Terraform plan:
+
+    terraform apply
+
+### Terraform output
+
+The console will output the public IP address of the EC2 instance after execution of `terraform apply`. Copy the IP address and paste into browser to visit EC2 instance over the public internet. The instance will be unreachable, until after we run the Ansible playbook and configure the EC2 instance.
---
+++ AWS/EC2_hello_world_Ansible/main.tf
@@ -0,0 +1,197 @@
+# Create VPC
+resource "aws_vpc" "vpc_tf" {
+  cidr_block                       = var.address_space
+  instance_tenancy                 = "default"
+  assign_generated_ipv6_cidr_block = true
+
+  tags = {
+    Name = "vpc-tf"
+  }
+}
+
+# Public web subnet in AZ A
+resource "aws_subnet" "sn_web_a_tf" {
+  vpc_id                  = aws_vpc.vpc_tf.id
+  cidr_block              = cidrsubnet(var.address_space, 4, 0)
+  availability_zone       = var.availability_zone_1
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "sn-web-a-tf"
+  }
+}
+
+# Private app subnet in AZ A
+resource "aws_subnet" "sn_app_a_tf" {
+  vpc_id            = aws_vpc.vpc_tf.id
+  cidr_block        = cidrsubnet(var.address_space, 4, 1)
+  availability_zone = var.availability_zone_1
+
+  tags = {
+    Name = "sn-app-a-tf"
+  }
+}
+
+# Private db subnet in AZ A
+resource "aws_subnet" "sn_db_a_tf" {
+  vpc_id            = aws_vpc.vpc_tf.id
+  cidr_block        = cidrsubnet(var.address_space, 4, 2)
+  availability_zone = var.availability_zone_1
+
+  tags = {
+    Name = "sn-db-a-tf"
+  }
+}
+
+# Private reserved subnet in AZ A
+resource "aws_subnet" "sn_reserved_a_tf" {
+  vpc_id            = aws_vpc.vpc_tf.id
+  cidr_block        = cidrsubnet(var.address_space, 4, 3)
+  availability_zone = var.availability_zone_1
+
+  tags = {
+    Name = "sn-reserved-a-tf"
+  }
+}
+
+# Public web subnet in AZ B
+resource "aws_subnet" "sn_web_b_tf" {
+  vpc_id                  = aws_vpc.vpc_tf.id
+  cidr_block              = cidrsubnet(var.address_space, 4, 4)
+  availability_zone       = var.availability_zone_2
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "sn-web-b-tf"
+  }
+}
+
+# Private app subnet in AZ B
+resource "aws_subnet" "sn_app_b_tf" {
+  vpc_id            = aws_vpc.vpc_tf.id
+  cidr_block        = cidrsubnet(var.address_space, 4, 5)
+  availability_zone = var.availability_zone_2
+
+  tags = {
+    Name = "sn-app-b-tf"
+  }
+}
+
+# Private db subnet in AZ B
+resource "aws_subnet" "sn_db_b_tf" {
+  vpc_id            = aws_vpc.vpc_tf.id
+  cidr_block        = cidrsubnet(var.address_space, 4, 6)
+  availability_zone = var.availability_zone_2
+
+  tags = {
+    Name = "sn-db-b-tf"
+  }
+}
+
+# Private reserved subnet in AZ B
+resource "aws_subnet" "sn_reserved_b_tf" {
+  vpc_id            = aws_vpc.vpc_tf.id
+  cidr_block        = cidrsubnet(var.address_space, 4, 7)
+  availability_zone = var.availability_zone_2
+
+  tags = {
+    Name = "sn-reserved-b-tf"
+  }
+}
+
+# Internet gateway attached to new VPC
+resource "aws_internet_gateway" "vpc_igw_tf" {
+  vpc_id = aws_vpc.vpc_tf.id
+
+  tags = {
+    Name = "vpc-igw-tf"
+  }
+}
+
+# Security group to access instances inside the public subnets using HTTP
+resource "aws_security_group" "ec2_public_access_tf" {
+  name        = "OtisEC2PublicAccess"
+  description = "Allow HTTP inbound"
+  vpc_id      = aws_vpc.vpc_tf.id
+
+  ingress {
+    description = "SSH from all IPv4"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "HTTPS to VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "ec2_ssh_key_tf" {
+  key_name   = "ssh-key-tf"
+  public_key = file("ssh-key-tf.pub")
+}
+
+# Route table for web subnets
+resource "aws_route_table" "rt_web_igw_tf" {
+  vpc_id = aws_vpc.vpc_tf.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.vpc_igw_tf.id
+  }
+
+  tags = {
+    Name = "rt-web-igw-tf"
+  }
+}
+
+# Associate route table with web a subnet
+resource "aws_route_table_association" "rt_associate_web_a_tf" {
+  subnet_id      = aws_subnet.sn_web_a_tf.id
+  route_table_id = aws_route_table.rt_web_igw_tf.id
+}
+
+# Associate route table with web b subnet
+resource "aws_route_table_association" "rt_associate_web_b_tf" {
+  subnet_id      = aws_subnet.sn_web_b_tf.id
+  route_table_id = aws_route_table.rt_web_igw_tf.id
+}
+
+resource "aws_network_interface" "eni_a_tf" {
+  subnet_id       = aws_subnet.sn_web_a_tf.id
+  private_ips     = ["10.17.0.100"]
+  security_groups = [aws_security_group.ec2_public_access_tf.id]
+
+  tags = {
+    Name = "ec2-web-a-eni-tf"
+  }
+}
+
+resource "aws_instance" "ec2_tf" {
+  ami           = lookup(var.aws_amis, var.aws_region)
+  instance_type = var.ec2_instance_type
+  key_name      = aws_key_pair.ec2_ssh_key_tf.key_name
+
+  network_interface {
+    network_interface_id = aws_network_interface.eni_a_tf.id
+    device_index         = 0
+  }
+
+  credit_specification {
+    cpu_credits = "unlimited"
+  }
+
+}
