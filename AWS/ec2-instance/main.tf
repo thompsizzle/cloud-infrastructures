4910ed6fdde7470ae8cc50c84d0d94cd19383473
thompsizzle <thompsizzle@gmail.com>
2022-11-27 13:34:33 -0700

    add local variable for resource tags

--- AWS/ec2-instance/main.tf
+++ AWS/ec2-instance/main.tf
@@ -1,114 +1,83 @@
-# Create VPC
 resource "aws_vpc" "vpc_tf" {
   cidr_block                       = var.address_space
   instance_tenancy                 = "default"
   assign_generated_ipv6_cidr_block = true
 
-  tags = {
-    Name = "vpc-tf"
-  }
+  tags = local.common_tags
 }
 
-# Public web subnet in AZ A
 resource "aws_subnet" "sn_web_a_tf" {
   vpc_id                  = aws_vpc.vpc_tf.id
   cidr_block              = cidrsubnet(var.address_space, 4, 0)
   availability_zone       = var.availability_zone_1
   map_public_ip_on_launch = true
 
-  tags = {
-    Name = "sn-web-a-tf"
-  }
+  tags = local.common_tags
 }
 
-# Private app subnet in AZ A
 resource "aws_subnet" "sn_app_a_tf" {
   vpc_id            = aws_vpc.vpc_tf.id
   cidr_block        = cidrsubnet(var.address_space, 4, 1)
   availability_zone = var.availability_zone_1
 
-  tags = {
-    Name = "sn-app-a-tf"
-  }
+  tags = local.common_tags
 }
 
-# Private db subnet in AZ A
 resource "aws_subnet" "sn_db_a_tf" {
   vpc_id            = aws_vpc.vpc_tf.id
   cidr_block        = cidrsubnet(var.address_space, 4, 2)
   availability_zone = var.availability_zone_1
 
-  tags = {
-    Name = "sn-db-a-tf"
-  }
+  tags = local.common_tags
 }
 
-# Private reserved subnet in AZ A
 resource "aws_subnet" "sn_reserved_a_tf" {
   vpc_id            = aws_vpc.vpc_tf.id
   cidr_block        = cidrsubnet(var.address_space, 4, 3)
   availability_zone = var.availability_zone_1
 
-  tags = {
-    Name = "sn-reserved-a-tf"
-  }
+  tags = local.common_tags
 }
 
-# Public web subnet in AZ B
 resource "aws_subnet" "sn_web_b_tf" {
   vpc_id                  = aws_vpc.vpc_tf.id
   cidr_block              = cidrsubnet(var.address_space, 4, 4)
   availability_zone       = var.availability_zone_2
   map_public_ip_on_launch = true
 
-  tags = {
-    Name = "sn-web-b-tf"
-  }
+  tags = local.common_tags
 }
 
-# Private app subnet in AZ B
 resource "aws_subnet" "sn_app_b_tf" {
   vpc_id            = aws_vpc.vpc_tf.id
   cidr_block        = cidrsubnet(var.address_space, 4, 5)
   availability_zone = var.availability_zone_2
 
-  tags = {
-    Name = "sn-app-b-tf"
-  }
+  tags = local.common_tags
 }
 
-# Private db subnet in AZ B
 resource "aws_subnet" "sn_db_b_tf" {
   vpc_id            = aws_vpc.vpc_tf.id
   cidr_block        = cidrsubnet(var.address_space, 4, 6)
   availability_zone = var.availability_zone_2
 
-  tags = {
-    Name = "sn-db-b-tf"
-  }
+  tags = local.common_tags
 }
 
-# Private reserved subnet in AZ B
 resource "aws_subnet" "sn_reserved_b_tf" {
   vpc_id            = aws_vpc.vpc_tf.id
   cidr_block        = cidrsubnet(var.address_space, 4, 7)
   availability_zone = var.availability_zone_2
 
-  tags = {
-    Name = "sn-reserved-b-tf"
-  }
+  tags = local.common_tags
 }
 
-# Internet gateway attached to new VPC
 resource "aws_internet_gateway" "vpc_igw_tf" {
   vpc_id = aws_vpc.vpc_tf.id
 
-  tags = {
-    Name = "vpc-igw-tf"
-  }
+  tags = local.common_tags
 }
 
-# Security group to access instances inside the public subnets using HTTP
 resource "aws_security_group" "ec2_public_access_tf" {
   name        = "EC2PublicAccessTF"
   description = "Allow HTTP inbound"
@@ -144,7 +113,6 @@ resource "aws_key_pair" "ec2_ssh_key_tf" {
   public_key = file("ssh-key-tf.pub")
 }
 
-# Route table for web subnets
 resource "aws_route_table" "rt_web_igw_tf" {
   vpc_id = aws_vpc.vpc_tf.id
 
@@ -153,18 +121,14 @@ resource "aws_route_table" "rt_web_igw_tf" {
     gateway_id = aws_internet_gateway.vpc_igw_tf.id
   }
 
-  tags = {
-    Name = "rt-web-igw-tf"
-  }
+  tags = local.common_tags
 }
 
-# Associate route table with web a subnet
 resource "aws_route_table_association" "rt_associate_web_a_tf" {
   subnet_id      = aws_subnet.sn_web_a_tf.id
   route_table_id = aws_route_table.rt_web_igw_tf.id
 }
 
-# Associate route table with web b subnet
 resource "aws_route_table_association" "rt_associate_web_b_tf" {
   subnet_id      = aws_subnet.sn_web_b_tf.id
   route_table_id = aws_route_table.rt_web_igw_tf.id
@@ -175,9 +139,7 @@ resource "aws_network_interface" "eni_a_tf" {
   private_ips     = ["10.17.0.100"]
   security_groups = [aws_security_group.ec2_public_access_tf.id]
 
-  tags = {
-    Name = "ec2-web-a-eni-tf"
-  }
+  tags = local.common_tags
 }
 
 resource "aws_instance" "ec2_tf" {
@@ -197,15 +159,15 @@ resource "aws_instance" "ec2_tf" {
   monitoring = var.ec2_instance_monitoring
 
   user_data = templatefile("user_data.tftpl", {})
+
+  tags = local.common_tags
 }
 
 resource "aws_s3_bucket" "bucket_tf" {
   bucket        = var.s3_state_bucket
   force_destroy = true
 
-  tags = {
-    Name = var.s3_state_bucket
-  }
+  tags = local.common_tags
 }
 
 resource "aws_s3_bucket_versioning" "versioning_tf" {
